8154093a4fce2bfb12906f9b27b17e60c4e32cd0
= <=>
2016-06-01 15:06:07 +0100

    second commit

---
+++ navigation.php
@@ -0,0 +1,11 @@
+<div class="header-container">
+    <header class="wrapper clearfix">
+        <h1 class="title">Coursework Hack</h1>
+            <nav>
+                <ul>
+                    <li <?php if($current == 'home') {echo 'class="current"';} ?>><a href="index.php">Home</a></li>
+                    <li <?php if($current == 'user profile') {echo 'class="current"';} ?>><a href="user.php">User Profile</a></li>               
+                </ul>
+            </nav>
+    </header>
+</div>
---
+++ user.php
@@ -0,0 +1,47 @@
+<?php 
+    // load up index.php
+
+    $current = 'user profile';
+
+    include("header.php");
+?>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <?php include("navigation.php"); ?>
+
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+
+                <article>
+                    <header>
+                        <h1>User Profile</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec.</p>
+                    </header>
+                </article>
+
+                <?php include("sidebar.php"); ?>
+
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
+
+        <?php include("footer.php") ?>
+
+            <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+            <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+            <script src="js/plugins.js"></script>
+            <script src="js/main.js"></script>
+
+            <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+            <script>
+                (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+                function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+                e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+                e.src='//www.google-analytics.com/analytics.js';
+                r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+                ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+            </script>
+    </body>
+</html>
